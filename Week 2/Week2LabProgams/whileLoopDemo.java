dfd9f669d260f7cf5b1dee02833c5db7ff138983
Mohamed Nazeer A <42862748+nazeerwahab@users.noreply.github.com>
2021-07-07 15:38:59 +0530

    Add files via upload

---
+++ Week 2/Week2LabProgams/whileLoopDemo.java
@@ -0,0 +1,13 @@
+package week2lab.programs;
+
+public class whileLoopDemo {
+
+	public static void main(String[] args) {
+		int x = 1, sum=0;
+		while(x<=10) {
+			sum=sum+x;
+			x++;
+		}
+		System.out.println("Summation: " + sum);
+	}
+}
