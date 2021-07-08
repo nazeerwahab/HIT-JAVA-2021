0fe2212b12882f75fab73ce2c3931ffde561d447
Mohamed Nazeer A <42862748+nazeerwahab@users.noreply.github.com>
2021-07-08 10:07:08 +0530

    Add files via upload

---
+++ Day 29/hit.day29/ColDemo4.java
@@ -0,0 +1,45 @@
+package hit.day29;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+//homework - stack and queue
+public class ColDemo4 {
+	public static void main(String[] args) {
+		//Set<String> set=new HashSet<String>();
+		Set<String> set=new TreeSet<String>(new MyComparator());
+		set.add("hello");
+		set.add("hai");
+		set.add("hello");
+		set.add("world");
+		set.add("earth");
+		
+		System.out.println(set);
+		set.add("zebra");
+		set.add("almighty");
+		
+		System.out.println(set);
+		
+		Iterator iter=set.iterator();
+		while(iter.hasNext()) {
+			System.out.println(iter.next());
+		}
+		
+		for(String s:set) {
+			System.out.println(s);
+		}
+		int i=10;
+		i+=10;//i=i+10;
+		System.out.println(i);
+	
+		
+	}
+}
+class MyComparator implements Comparator<String>{
+	@Override
+	public int compare(String o1, String o2) {
+		// TODO Auto-generated method stub
+		return o2.compareTo(o1);
+	}
+}
